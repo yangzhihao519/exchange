e010d60bcde1c3c410df4c6487ff0807e29dacac
Zhihao Yang <yang_zhihao@live.com>
2019-09-22 23:28:00 +0100

    Updates

---
+++ precache-manifest.d8ac4fe8453dee0cd43611315452091c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df72b94c89534df30f27269eca1604bf",
+    "url": "/exchange/index.html"
+  },
+  {
+    "revision": "3234ec2fc32d234fb1ff",
+    "url": "/exchange/static/css/main.1b138d8f.chunk.css"
+  },
+  {
+    "revision": "634b7aea86943ad9e55d",
+    "url": "/exchange/static/js/2.aafc9456.chunk.js"
+  },
+  {
+    "revision": "3234ec2fc32d234fb1ff",
+    "url": "/exchange/static/js/main.4d614305.chunk.js"
+  },
+  {
+    "revision": "935dbc5b78d62da9ac6a",
+    "url": "/exchange/static/js/runtime~main.77471482.js"
+  }
+]);
